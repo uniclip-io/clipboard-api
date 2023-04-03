113bc1ba7daf350e0f695caf17b9ac622a495aca
noahgreff <greffnoah@gmail.com>
2023-04-03 21:27:59 +0200

    added clipboard service

---
+++ ClipboardService/Services/ClipboardService.cs
@@ -0,0 +1,40 @@
+using ClipboardService.Models;
+using ClipboardService.Repositories;
+using ClipboardService.Repositories.Contracts;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ClipboardService.Services;
+
+public class ClipboardService
+{
+    private readonly ClipboardRepository _clipboardRepository;
+    private readonly RecordRepository _recordRepository;
+
+    public ClipboardService(ClipboardRepository clipboardRepository, RecordRepository recordRepository)
+    {
+        _clipboardRepository = clipboardRepository;
+        _recordRepository = recordRepository;
+    }
+
+    public async Task<Clipboard> CreateClipboard(Guid userId)
+    {
+        var clipboardContract = await _clipboardRepository.CreateClipboardForUser(userId);
+        return new Clipboard(clipboardContract.Id, new List<Record>());
+    }
+    
+    public async Task<Clipboard> GetClipboardByUserId(Guid userId)
+    {
+        var clipboardContract = await _clipboardRepository.GetClipboardByUserId(userId);
+        var recordContracts = await _recordRepository.GetRecordsByClipboardId(clipboardContract.Id);
+        var records = recordContracts.Select(r => new Record(r.Id, r.Date, r.ContentType, r.Content)).ToList();
+        
+        return new Clipboard(clipboardContract.Id, records);
+    }
+
+    public async Task<Record> AddContentToClipboard(Guid clipboardId, string contentType, string content)
+    {
+        var recordContract = await _recordRepository.AddRecordToClipboard(clipboardId, contentType, content);
+
+        return new Record(recordContract.Id, recordContract.Date, recordContract.ContentType, recordContract.Content);
+    }
+}
